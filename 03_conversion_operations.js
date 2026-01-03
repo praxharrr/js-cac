544c67095411e79320882fcc9835be6bf8e5bf37
Prakhar Sharma <praxhar001@gmail.com>
2026-01-03 11:41:46 +0530

    Enhance conversion and arithmetic operations examples
    Added detailed comments and examples for conversion operations and arithmetic operations.

--- 03_conversion_operations.js
+++ 03_conversion_operations.js
@@ -25,3 +25,46 @@ console.log(booleanIsLoggedIn)
 // Boolean(null) => false
 // Boolean(undefined) => false
 // Boolean(NaN) => false
+
+//**************OPERATIONS******************/
+let value = 3
+let negValue = -value
+console.log(negValue)
+// -3
+
+console.log(2+2)
+console.log(2-2)
+console.log(2*2)
+console.log(2/2)
+console.log(2**2) // exponentiation operator
+console.log(5%2) //modulus operator
+
+let str1 = "Hello"
+let str2 = "Teesha"
+let str3 = str1 +" "+ str2
+console.log(str3) // Hello Teesha'
+
+// concatenation operator
+
+console.log("2" + 2) // "22"
+console.log("2" + "2") // "22"
+console.log(2  +"2") // "22"
+console.log(2 + 2 + "2") // "42"
+console.log("2" + 2 + 2) // "222"
+
+console.log("Prakhar" + "Teesha") // "PrakharTeesha"
+
+//prefix and postfix increment and decrement operators
+let increment = 5
+console.log(++increment) //6
+console.log(increment) //6
+console.log(increment++) //6
+console.log(increment) //7
+
+let decrement = 5
+console.log(--decrement) //4
+console.log(decrement) //4
+console.log(decrement--) //4
+console.log(decrement) //3
+//prefix increments/decrements the value first and then uses it
+//postfix uses the value first and then increments/decrements it
