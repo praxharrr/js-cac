2c9656e25cd321e818518b15e38bc8fa588acb66
Prakhar Sharma <praxhar001@gmail.com>
2026-05-23 11:58:17 +0530

    Add files via upload

---
+++ math.js
@@ -0,0 +1,13 @@
+const sum = (a,b) => a+b;
+const multiply = (a,b) => a*b;
+const g = 9.8;
+const pi = 3.14;
+
+let obj = {
+    sum: sum,
+    multiply: multiply,
+    g: g,
+    pi: pi
+}
+
+module.exports = obj;
---
+++ script.js
@@ -0,0 +1,15 @@
+//let n = 5;
+//
+//for(let i =0; i<n ; i++){
+//    console.log("Hello", i);
+//}
+//
+//console.log("bye");
+
+//console.log(process.argv);
+const someValue = require('./math');
+console.log(someValue);
+
+
+
+
